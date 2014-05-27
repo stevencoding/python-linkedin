1de666194cd64db79836af0c092b9a5d5c6772b9
Steven <stevencoding@gmail.com>
2014-05-27 00:29:35 -0700

    return important info of a post

--- linkedin/linkedin_group_extra.py
+++ linkedin/linkedin_group_extra.py
@@ -3,9 +3,8 @@ from linkedin import LinkedInApplication, LinkedInSelector, ENDPOINTS
 from utils import *
 
 GROUP_SELECTORS = ("id,category,creation-timestamp,title,summary,"
-"creator:(first-name,last-name,picture-url,headline),"
-"likes,attachment:(image-url,content-domain,content-url,title,summary),"
-"relation-to-viewer")
+"creator:(id),"
+"likes,attachment:(content-url,title,summary)")
 
 class LinkedInGroupExtra(LinkedInApplication):
     def get_posts(self, group_id, post_ids=None, selectors=None, params=None,
@@ -15,15 +14,27 @@ class LinkedInGroupExtra(LinkedInApplication):
              url = '%s::(%s)' % (url, ','.join(map(str, post_ids)))
         if selectors:
             url = '%s:(%s)' % (url, LinkedInSelector.parse(selectors))
-        print url
+        # print url
         response = self.make_request('GET', url, params=params, headers=headers)
         raise_for_error(response)
         return response.json()
 
     def decompose_post(self, group_id, post_id, params=None, headers=None):
         data = self.get_posts(group_id, {post_id}, GROUP_SELECTORS, params, headers)
-        print data
+        # print data
+        post_abs_id = data['values'][0]['id']
+        # print "post_abs_id = "+post_abs_id
+        creator_id = data['values'][0]['creator']['id']
+        # print "creator_id = "+creator_id
         title = data['values'][0]['title']
-        # print title
-        return title
+        category = data['values'][0]['category']['code']
+        summary = data['values'][0]['summary']
+        attachment_title = data['values'][0]['attachment']['title']
+        attachment_summary = data['values'][0]['attachment']['summary']
+        # print "title is: "+title
+        # print "category is: "+category
+        # print "summary is: "+summary
+        # print "attachment_title is: "+attachment_title
+        # print "attachment_summary is: "+attachment_summary
+        return post_abs_id, creator_id, title, category, summary, attachment_title, attachment_summary
 
