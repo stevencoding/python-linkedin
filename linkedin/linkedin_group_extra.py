8edf833c449fe1a89dc337afe1595d2f3bf72553
Steven <stevencoding@gmail.com>
2014-05-26 23:57:27 -0700

    use selectors to specify what info to return in json

--- linkedin/linkedin_group_extra.py
+++ linkedin/linkedin_group_extra.py
@@ -1,25 +1,27 @@
 # -*- coding: utf-8 -*-
-from linkedin import *
+from linkedin import LinkedInApplication, LinkedInSelector, ENDPOINTS
 from utils import *
 
-ENDPOINTS_GROUPS = 'https://api.linkedin.com/v1/groups'
-GROUP_POST_REQUEST_DETAILED = "posts:(creation-timestamp,title,summary,creator:(first-name,last-name,picture-url,headline),likes,attachment:(image-url,content-domain,content-url,title,summary),relation-to-viewer)"
+GROUP_SELECTORS = ("id,category,creation-timestamp,title,summary,"
+"creator:(first-name,last-name,picture-url,headline),"
+"likes,attachment:(image-url,content-domain,content-url,title,summary),"
+"relation-to-viewer")
 
 class LinkedInGroupExtra(LinkedInApplication):
     def get_posts(self, group_id, post_ids=None, selectors=None, params=None,
                   headers=None):
-        url = '%s/%s/posts' % (ENDPOINTS_GROUPS, str(group_id))
+        url = '%s/%s/posts' % (ENDPOINTS.GROUPS, str(group_id))
         if post_ids:
-            url = '%s::(%s)' % (url, ','.join(map(str, post_ids)))
+             url = '%s::(%s)' % (url, ','.join(map(str, post_ids)))
         if selectors:
             url = '%s:(%s)' % (url, LinkedInSelector.parse(selectors))
+        print url
         response = self.make_request('GET', url, params=params, headers=headers)
         raise_for_error(response)
         return response.json()
 
-    def decompose_post(self, group_id, post_id, selectors=None, params=None,
-                  headers=None):
-        data = self.get_posts(group_id, {post_id}, selectors, params, headers)
+    def decompose_post(self, group_id, post_id, params=None, headers=None):
+        data = self.get_posts(group_id, {post_id}, GROUP_SELECTORS, params, headers)
         print data
         title = data['values'][0]['title']
         # print title
