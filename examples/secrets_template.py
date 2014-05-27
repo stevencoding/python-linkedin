21ba753532e200f4d2ac7637f827abb4a4d96e18
Steven <stevencoding@gmail.com>
2014-05-26 17:07:09 -0700

    template for secrets.py

---
+++ examples/secrets_template.py
@@ -0,0 +1,7 @@
+# change this file name to secrets.py, and fill in following xxx
+
+secret = {'API_KEY':"xxx",
+			'Secret_Key':"xxx",
+			'OAUT':"xxx",
+			'OAUS':"xxx",
+            'Group_ID':xxx}
